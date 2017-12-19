b6898e18c97f548bfb42059b5291aac69f2e7d86
AlianWen <443129953@qq.com>
2017-12-19 09:27:29 +0800

    Update models.py

--- DjangoProd/LearningLogs/models.py
+++ DjangoProd/LearningLogs/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Topic(models.Model):
     """Learning Topic"""
     text=models.CharField(max_length=200)
     date_added=models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User,on_delete=models.DO_NOTHING)
     
     def __str__(self):
         return self.text
