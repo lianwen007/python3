d02a69f35454b4b5c3e9dd7364714b95ad301331
AlianWen <443129953@qq.com>
2017-12-20 16:49:43 +0800

    Create MySqlInsert.py

---
+++ DataBase/MySql/MySqlInsert.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 20 16:37:00 2017
+
+@author: Alian
+"""
+
+import pymysql
+
+db=pymysql.connect(host="localhost",port=3306,user="root",passwd="123",db="elasticsearch",charset="utf8")
+cursor=db.cursor()
+sql="""insert into test02(
+SELECT * FROM test01)"""
+
+try:
+    cursor.execute(sql)
+    db.commit()
+except:
+    print("Error")
+db.close()  
