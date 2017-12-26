1f678f0c17822788da63c2378a1f455d820d4dc0
AlianWen <443129953@qq.com>
2017-12-26 09:06:28 +0800

    Create Mysql2Hive.cmd

---
+++ DataBase/Sqoop/Mysql2Hive.cmd
@@ -0,0 +1,8 @@
+
+sqoop create-hive-table --connect jdbc:mysql://172.16.10.26:3306/xh_elasticsearch --table xhschool_info --username xhelas --password *** --hive-table xhschool_info
+
+sqoop import --connect jdbc:mysql://172.16.10.26:3306/xh_elasticsearch --username xhelas --password *** --table xhschool_info --hive-import --hive-table xhschool_info
+
+
+
+-incremental lastmodified -check-column created -last-value ‘2012-02-01 11:0:00′
