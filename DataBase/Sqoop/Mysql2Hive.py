d9836ba893d4498400aa062e6e66bd886a61730f
AlianWen <443129953@qq.com>
2018-01-11 10:24:18 +0800

    Update Mysql2Hive.py

--- DataBase/Sqoop/Mysql2Hive.py
+++ DataBase/Sqoop/Mysql2Hive.py
@@ -1,7 +1,7 @@
 
-sqoop create-hive-table --connect jdbc:mysql://172.16.10.26:3306/xh_elasticsearch --table xhschool_info --username xhelas --password *** --hive-table xhschool_info
+sqoop create-hive-table --connect jdbc:mysql://172.16.10.26:3306/xh_elasticsearch --table xhschool_info --username *** --password *** --hive-table xhschool_info
 
-sqoop import --connect jdbc:mysql://172.16.10.26:3306/xh_elasticsearch --username xhelas --password *** --table xhschool_info --hive-import --hive-table xhschool_info
+sqoop import --connect jdbc:mysql://172.16.10.26:3306/xh_elasticsearch --username *** --password *** --table xhschool_info --hive-import --hive-table xhschool_info
 
 
 
