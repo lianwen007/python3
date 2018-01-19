093f658a19b84a81f00da197e1273ed597ba0869
AlianWen <443129953@qq.com>
2018-01-19 15:34:01 +0800

    Update APIRunGetUserid.py

--- DataBase/APIRunGetUserid.py
+++ DataBase/APIRunGetUserid.py
@@ -3,9 +3,9 @@ import pymysql
 import json
 
 app = Flask(__name__)#创建一个服务，赋值给APP
-@app.route('/get_user',methods=['post'])#指定接口访问的路径，支持什么请求方式get，post
+@app.route('/bigdata/user_file_product/getuserid',methods=['post'])#指定接口访问的路径，支持什么请求方式get，post
 def get_ss():
-    userid = request.args.get('userid')#使用request.args.get方式获取拼接的入参数据
+    userid = request.args.get('userid') #使用request.args.get方式获取拼接的入参数据
     #userid = request.json.get('userid') #获取带json串请求的userid参数传入的值
     #userid = request.values.get('userid') #支持获取连接拼接的参数，而且还能获取body form填入的参数
     data=get_mysql_conn(userid)
@@ -19,8 +19,14 @@ def get_mysql_conn(userid=''):
     mess={}
     try:
         cursor.execute(sql)
-        results=cursor.fetchone()
-        #mess['id']=results[0]
+        result=cursor.fetchone()
+        results=[]
+        for x in range(19): #空值处理为0
+            if result[x]is not None:
+                results.append(result[x])
+            else:
+                results.append(0)
+                
         mess['userid']=results[0]
         mess['username']=results[1]
         mess['schoolid']=results[2]
