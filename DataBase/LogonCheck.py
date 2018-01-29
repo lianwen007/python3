5055cadb6e6c273bb768a73fed7cf6bbb57c5491
AlianWen <443129953@qq.com>
2018-01-29 09:57:46 +0800

    Update LogonCheck.py

--- DataBase/LogonCheck.py
+++ DataBase/LogonCheck.py
@@ -9,7 +9,7 @@ import pymysql
 class Userlogin(object):
     
     def __init__(self,username,password):
-        self.username=username
+        self.username=str(username)
         self.password=password.upper()
         self.conn=pymysql.connect(host="localhost",port=3306,
                            user="root",passwd="123",db="elasticsearch",charset="utf8")
