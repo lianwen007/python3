ed85f8d6c99be6213b78dfad1cde6c0f73f478e4
AlianWen <443129953@qq.com>
2018-02-01 21:51:13 +0800

    Update APIGetStwInfo.py

--- DataBase/MyAPI/APIGetStwInfo.py
+++ DataBase/MyAPI/APIGetStwInfo.py
@@ -12,22 +12,19 @@ app = Flask(__name__)#创建一个服务，赋值给APP
 @app.route('/bigdata/product_stw/getstwinfo',methods=['post'])#指定接口访问的路径，支持什么请求方式get，post
 def get_ss():
     #userid = request.args.get('userid') #使用request.args.get方式获取拼接的入参数据
-    schoolid = request.json.get('schoolid') #获取带json串请求的userid参数传入的值
+    schoolid = str(request.json.get('schoolid')) #获取带json串请求的userid参数传入的值
     starttime = request.json.get('starttime')
     endedtime = request.json.get('endedtime')
     #userid = request.values.get('userid') #支持获取连接拼接的参数，而且还能获取body form填入的参数
     s=Getstwinfo(schoolid,starttime,endedtime)
-    return json.dumps(s.main() , ensure_ascii=False) 
-
+    return schoolid+json.dumps(s.main() , ensure_ascii=False)
 
 class Getstwinfo(object):
     def __init__(self,schoolid,starttime,endedtime):
-        self.schoolid=schoolid
+        self.schoolid=str(schoolid)
         self.starttime=starttime
         self.endedtime=endedtime
-        self.conn=connect(host='172.10.16.141', port=21050,timeout=3600)
-
-
+        self.conn=connect(host='nat.yunzuoye.net', port=6667,timeout=3600)
     def getkinginfo(self):
         descnames=['userid','username','schoolid','schoolname','classname','classid',
         'bookname','bookid','hp','credit','countscore','numhomework','numselfwork',
@@ -36,10 +33,10 @@ class Getstwinfo(object):
                 and (unix_timestamp(`datetime`)>=%s and unix_timestamp(`datetime`)<=%s)"%(self.schoolid,self.starttime,self.endedtime)
         cur = self.conn.cursor()
         cur.execute(sqlsel)
-        mess={}
         messes=[]
         data_list=cur.fetchall()
         for datas in data_list:
+            mess={}
             for x in range(len(datas)):
                 mess[descnames[x]]=datas[x]
             messes.append(mess)
@@ -52,7 +49,7 @@ class Getstwinfo(object):
             data['msg']='successful!'
         return data
 
-app.run(host='0.0.0.0',port=18890,debug=True,threaded=True)
+app.run(host='0.0.0.0',port=8802,debug=True,threaded=True)
 
 
 #from impala.dbapi import connect
