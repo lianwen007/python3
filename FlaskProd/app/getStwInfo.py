bd80685e2fc963ff264db97fb8dff01d5b65bb6b
AlianWen <443129953@qq.com>
2018-03-13 17:23:40 +0800

    Update and rename getstwinfo.py to getStwInfo.py

--- FlaskProd/app/getStwInfo.py
+++ FlaskProd/app/getStwInfo.py
@@ -1,15 +1,17 @@
+# 0313-日志结构变更，规范标题大小写
 # 0307-增加HP等字段的实时调用接口
+
 from flask import Blueprint, render_template, redirect,request
 from app import app
 from .relog import log
 #from .models import Stwdaycount
-import json,time,requests
+import json,time,requests,uuid
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
-getstwinfo = Blueprint('getstwinfo',__name__)
+getStwInfo = Blueprint('getStwInfo',__name__)
 
-@getstwinfo.route('/bigdata/product_stw/getstwinfo', methods=['post'])  # 指定接口访问的路径，支持什么请求方式get，post
+@getStwInfo.route('/bigdata/product_stw/getstwinfo', methods=['post'])  # 指定接口访问的路径，支持什么请求方式get，post
 def get_stwinfo():
     # userid = request.args.get('userid') #使用request.args.get方式获取拼接的入参数据
     schoolid = request.json.get('schoolid')  # 获取带json串请求的userid参数传入的值
@@ -19,6 +21,7 @@ def get_stwinfo():
     classid = request.json.get('classid',0)
     gettype = request.json.get('gettype',0)
     # userid = request.values.get('userid') #支持获取连接拼接的参数，而且还能获取body form填入的参数
+    traceId = str(uuid.uuid1()).replace('-', '')
     if starttime is None or starttime == 0:
         starttime = int(time.time()) - 24*60*60*8
     if endedtime is None or endedtime == 0:
@@ -26,26 +29,20 @@ def get_stwinfo():
     if bookid is None or bookid == 0:
         bookid = '%'
     if gettype == 'getbookid':
-        s = Getbookid(schoolid)
-        if schoolid:
-            logInfogetbk =  'type[' + str(gettype) + ']' + 'scid['+schoolid+']'
-        else:
-            logInfogetbk =  'type[' + str(gettype) + ']' + 'scid[All]'
+        s = Getbookid(schoolid,traceId)
     else:
-        s = Getstwinfo(schoolid, starttime, endedtime, bookid, classid)
-        logInfogetbk= 'type[getinfo]' + 'scid['+schoolid+']'
-    logInfo=logInfogetbk+'sttime['+str(starttime)+']'+'endtime['+str(endedtime)+']' +'bkid['+str(bookid)+']'+'clid['+str(classid)+']'
-    log('logInfo:APIRequest - ', logInfo)
+        s = Getstwinfo(schoolid, starttime, endedtime, bookid, classid, traceId)
     return json.dumps(s.main(), ensure_ascii=False)
 
 class Getstwinfo(object):
-    def __init__(self, schoolid, starttime, endedtime, bookid, classid):
+    def __init__(self, schoolid, starttime, endedtime, bookid, classid, traceId):
         self.schoolid = str(schoolid)
         self.starttime = starttime
         self.endedtime = endedtime
         self.bookid = bookid
         self.classid = classid
         self.checkscid = ''
+        self.traceId=traceId
 
     def checkschoolid(self):
         sql = "select distinct schoolid from product_stw_daycount where schoolid in (%s) \
@@ -111,22 +108,28 @@ class Getstwinfo(object):
     def main(self):
         Getstwinfo.checkschoolid(self)
         data = {}
+        data['traceId']= self.traceId
         if self.checkscid == 'right':
             data['code'] = 200
             data['data'] = Getstwinfo.getkinginfo(self)
             data['msg'] = 'successful!'
         else:
-            data['code'] = -1
+            data['code'] = 400
             data['msg'] = 'These schools is not exist!'
-        log('logInfo:Getstwinfo - ', data['code'])
+        logInfo = str(data['code'])  + '[' + self.traceId + ']type[getStwinfo]' + 'scid[' + self.schoolid + ']'\
+                  + 'sttime[' + str(self.starttime) + ']' + 'endtime[' + str(self.endedtime) + ']' + 'bkid[' +\
+                str(self.bookid) + ']' + 'clid[' + str(self.classid) + ']'
+        log('APIRequest-', logInfo)
         return data
 
 class Getbookid(object):
-    def __init__(self, schoolid=None):
+    def __init__(self, schoolid, traceId):
         self.schoolid = schoolid
+        self.traceId = traceId
+
     def findallbookid(self):
-        sqlsel = "select distinct bookname,bookid from product_stw_daycount"
-        descnames = ['bookname', 'bookid']
+        sqlsel = "select distinct bookname,bookid,subtype from product_stw_subject"
+        descnames = ['bookname', 'bookid','subtype']
         data_list  = db.session.execute(sqlsel)
         messes = []
         for datas in data_list:
@@ -136,7 +139,7 @@ class Getbookid(object):
             messes.append(mess)
         return messes
     def findbookidbysc(self):
-        sqlsel = "select distinct book name,bookid from product_stw_daycount where schoolid in (%s)" % (
+        sqlsel = "select distinct bookname,bookid from product_stw_daycount where schoolid in (%s)" % (
             self.schoolid)
         descnames = ['bookname', 'bookid']
         data_list  = db.session.execute(sqlsel)
@@ -149,16 +152,19 @@ class Getbookid(object):
         return messes
 
     def main(self):
-        if self.schoolid:
-            datav = Getbookid.findbookidbysc(self)
-        else:
-            datav = Getbookid.findallbookid(self)
+        datav = Getbookid.findallbookid(self)
         data = {}
+        data['traceId'] = self.traceId
         data['code'] = 200
         data['data'] = datav
         data['msg'] = 'successful!'
-        log('logInfo:Getbookid - ', data['code'])
+        logInfo = str(data['code'])+'['+self.traceId + ']type[getBookid]' + 'scid[All]'
+        log('APIRequest-', logInfo)
+        # log('logInfo:Getbookid - ', data['code'])
         return data
-@getstwinfo.route('/bigdata/product_stw/getid', methods=['GET'])
+
+
+
+@getStwInfo.route('/bigdata/product_stw/getid', methods=['GET'])
 def getid():
     return 'test success'
