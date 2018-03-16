428772a010532beb0473380422617be1551dfe9f
AlianWen <443129953@qq.com>
2018-03-16 13:17:46 +0800

    Update __init__.py

--- FlaskProd/app/__init__.py
+++ FlaskProd/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, url_for, request, redirect, render_template
-
+from flask_cache import Cache
+#from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
+cache=Cache(app, config={'CACHE_TYPE':'simple'})
+#db = SQLAlchemy(app)
 from app import models,views
