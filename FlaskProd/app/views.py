ed8ae2715851d1fea7ce7d74d6d1b7b753f5e77f
AlianWen <443129953@qq.com>
2018-04-02 15:55:41 +0800

    Update views.py

--- FlaskProd/app/views.py
+++ FlaskProd/app/views.py
@@ -1,7 +1,9 @@
 from app import app
 from .getStwInfo import getStwInfo
 from .getKpiInfo import getKpiInfo
+from .getEnStwInfo import getEnStwInfo
 
 app.register_blueprint(getStwInfo,url_prefix='')
 app.register_blueprint(getKpiInfo,url_prefix='')
+app.register_blueprint(getEnStwInfo,url_prefix='')
 #app.register_blueprint(user, url_prefix='/user')
