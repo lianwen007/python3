619f5d00b20bbf7fe02ab31aacf3dcf5e982e88c
AlianWen <443129953@qq.com>
2018-04-03 15:49:09 +0800

    Update config.py

--- FlaskProd/config.py
+++ FlaskProd/config.py
@@ -1,3 +1,9 @@
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123@localhost:3306/elasticsearch?charset=utf8'
 #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://webmail_user:szw_webmail1234@172.16.10.27:3306/xh_elasticsearch?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+# mongo config
+mongo_url = "192.168.5.52:50000"
+etl_root_path = "/root/stwMongo/etl"
+etl_log_path = "/root/stwMongo/etl_log"
