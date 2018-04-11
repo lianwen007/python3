cef44263834acdae9c3f9ea16d67bdc38d5d4460
AlianWen <443129953@qq.com>
2018-04-11 09:06:05 +0800

    Add files via upload

---
+++ FlaskETLProd/config.py
@@ -0,0 +1,10 @@
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123@localhost:3306/elasticsearch?charset=utf8'
+SECRET_KEY = 'Asd!23Asd!23'
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://webmail_user:szw_webmail1234@172.16.10.26:3306/xh_elasticsearch?charset=utf8'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# mongo config
+mongo_url = "192.168.5.52:50000"
+etl_root_path = "/root/stwMongo/etl"
+etl_log_path = "/root/stwMongo/etl_log"
