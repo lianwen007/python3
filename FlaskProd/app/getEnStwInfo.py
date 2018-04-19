acc2eb9ee3f6fbf59d9cababe3a2d087e2c253fd
AlianWen <443129953@qq.com>
2018-04-19 13:24:49 +0800

    Update getEnStwInfo.py

--- FlaskProd/app/getEnStwInfo.py
+++ FlaskProd/app/getEnStwInfo.py
@@ -1,47 +1,52 @@
 from flask import Blueprint, render_template, redirect,request,jsonify
-from app import app,cache
+from app import app, cache
 from .relog import log
 #from .models import Stwdaycount
 import json,time,requests
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
-
 getEnStwInfo = Blueprint('getEnStwInfo', __name__)
+
+
 def getTraceId():
     import uuid
     return str(uuid.uuid1()).replace('-', '')
 
-@getEnStwInfo.route('/bigdata/product_stw/enstwInfo/getEnStwInfo', methods=['GET'])
+
+@getEnStwInfo.route('/getEnStwInfo', methods=['GET'])
 def get_enstw_info():
     schoolid = request.values.get('schoolId')
+    classid = request.values.get('classId', '0')
     starttime = request.values.get('startTime', int(time.time()) - 24*60*60*8)
     endedtime = request.values.get('endTime', int(time.time()))
     if not schoolid:
         traceid=getTraceId()
-        mess ={'code': 400, 'msg': 'Error! Please enter the correct schoolId!', 'traceId': traceid}
-        logInfo = '400' + '[' + traceid + ']type[getEnStwInfo]' + 'scid[Noid]' \
+        mess = {'code': 400, 'msg': 'Error! Please enter the correct schoolId!', 'traceId': traceid}
+        logInfo = '400' + '[' + traceid + ']type[getEnStwInfo]' + 'scid[Noid]clid[Noid]' \
                   +'sttime[' + str(starttime) + ']endtime[' + str(endedtime) + ']'
         log('APIRequest-', logInfo)
         return jsonify(mess)
     else:
-        s = GetEnStwInfo(schoolid, starttime, endedtime,)
+        s = GetEnStwInfo(schoolid, classid, starttime, endedtime,)
         return jsonify(s.main())
 
-@getEnStwInfo.route('/bigdata/product_stw/enstwInfo/clearCache', methods=['GET'])
+
+@getEnStwInfo.route('/clearCache', methods=['GET'])
 def enstw_cache_clear():
     password = request.values.get('password')
     if password == 'bigdata123':
         cache.clear()
-        datas = {'msg': 'Successful! Clear All'}
+        datas = {'msg': 'Succeed to clear all'}
     else:
         datas = {'msg': 'Error, Password was wrong!'}
     return jsonify(datas)
 
 class GetEnStwInfo(object):
-    def __init__(self, schoolid, starttime, endedtime,):
+    def __init__(self, schoolid, classid, starttime, endedtime,):
         self.schoolid = schoolid
         self.starttime = starttime
         self.endedtime = endedtime
+        self.classid = classid
         self.checkscid = ''
         self.alldescs = ['userId', 'userName', 'schoolId', 'classId', 'className']
 
@@ -55,10 +60,14 @@ class GetEnStwInfo(object):
 
     def get_enstw_info(self):
         @cache.memoize(timeout=3600 * 12)
-        def get_all_user(schoolid, alldescs):
+        def get_all_user(schoolid, classid, alldescs):
             # 获取学校的所有学生列表
-            sqlsel = "select DISTINCT userid,username,schoolid,classid,classname from teacher_student_info  " \
-                     "where classname!='教师' AND schoolid in (%s)" % (schoolid)
+            if classid == '0'or not classid:
+                sqlsel = "select DISTINCT userid,username,schoolid,classid,classname from teacher_student_info  " \
+                         "where classname!='教师' AND schoolid in (%s)" % (schoolid)
+            else:
+                sqlsel = "select DISTINCT userid,username,schoolid,classid,classname from teacher_student_info  " \
+                     "where classname!='教师' AND classid in (%s)" % (classid)
             data_lists = db.session.execute(sqlsel)
             messes = []
             for datas in data_lists:
@@ -70,11 +79,18 @@ class GetEnStwInfo(object):
 
         descnames = ['userId', 'userName', 'schoolId', 'classId', 'className', 'gameCount', 'finCount',
                      'homeFull', 'selfCount', 'selfFull', 'listenNum', 'readNum', 'blankNum']
-        sqlsel = "SELECT userid,username,schoolid,classid,classname,CAST(SUM(gamecount)AS SIGNED),CAST(SUM(finishcount)AS SIGNED),\
+        if not self.classid or self.classid == '0':
+            sqlsel = "SELECT userid,username,schoolid,classid,classname,CAST(SUM(gamecount)AS SIGNED),CAST(SUM(finishcount)AS SIGNED),\
                 CAST(SUM(homefull)AS SIGNED),CAST(SUM(selfcount)AS SIGNED),CAST(SUM(selffull)AS SIGNED),CAST(SUM(listennum)AS SIGNED),\
                  CAST(SUM(readnum)AS SIGNED),CAST(SUM(blanknum)AS SIGNED) FROM product_stw_encount WHERE schoolid=%s \
                   AND unix_timestamp(`datetime`)>=%s AND unix_timestamp(`datetime`)<=%s GROUP BY userid,username,schoolid,classid,classname" \
                  % (self.schoolid, self.starttime, self.endedtime)
+        else:
+            sqlsel = "SELECT userid,username,schoolid,classid,classname,CAST(SUM(gamecount)AS SIGNED),CAST(SUM(finishcount)AS SIGNED),\
+                          CAST(SUM(homefull)AS SIGNED),CAST(SUM(selfcount)AS SIGNED),CAST(SUM(selffull)AS SIGNED),CAST(SUM(listennum)AS SIGNED),\
+                          CAST(SUM(readnum)AS SIGNED),CAST(SUM(blanknum)AS SIGNED) FROM product_stw_encount WHERE classid=%s \
+                          AND unix_timestamp(`datetime`)>=%s AND unix_timestamp(`datetime`)<=%s GROUP BY userid,username,schoolid,classid,classname" \
+                     % (self.classid, self.starttime, self.endedtime)
         data_list = db.session.execute(sqlsel)
         messes = []
         for datas in data_list:
@@ -82,7 +98,10 @@ class GetEnStwInfo(object):
             for x in range(len(datas)):
                 mess[descnames[x]] = datas[x]
             messes.append(mess)
-        student_all=get_all_user(self.schoolid,self.alldescs)
+        try:
+            student_all = get_all_user(self.schoolid, self.classid, self.alldescs)
+        except:
+            student_all = []
         alldatas = []
         for allmess in student_all:
             for rdataf in messes:
@@ -107,7 +126,7 @@ class GetEnStwInfo(object):
         else:
             data['code'] = 400
             data['msg'] = 'Error! These schools are not exist!'
-        logInfo = str(data['code'])  + '[' + data['traceId'] + ']type[getEnStwInfo]' + 'scid[' + str(self.schoolid) \
-                   + ']sttime[' + str(self.starttime) + ']endtime[' + str(self.endedtime) + ']'
+        logInfo = str(data['code']) + '[' + data['traceId'] + ']type[getEnStwInfo]' + 'scid[' + str(self.schoolid) \
+                  + ']clid[' + str(self.classid) + ']sttime[' + str(self.starttime) + ']endtime[' + str(self.endedtime) + ']'
         log('APIRequest-', logInfo)
         return data
