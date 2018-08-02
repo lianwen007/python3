f28e3749b05ae252cfe0290148718aaed63e4ebe
AlianWen <443129953@qq.com>
2018-08-02 15:32:58 +0800

    Update PD_test.py

--- MyTestFile/PD_test.py
+++ MyTestFile/PD_test.py
@@ -7,3 +7,26 @@ rs = pd.DataFrame({'A':['我b45D','刘245D','刘123','刘123'],
                     'D':[0,3,5,2]})
                  
 rs['D'][(rs['A'].str.contains('刘')) & (rs['B']>=60)] = 1  
+
+
+# dict or list 转换为 dataframe
+tdata = [
+    {
+      "taskFixNum": 12, 
+      "taskStuNum": 600, 
+      "taskUploadNum": 50, 
+      "tchId": 70155
+    }, 
+    {
+      "taskFixNum": 12, 
+      "taskStuNum": 588, 
+      "taskUploadNum": 50, 
+      "tchId": 70155
+    }]
+trs = pd.DataFrame(tdata)
+
+dict_country = trs.to_json(orient='records')  
+
+# dataframe 转 json
+import json
+inp_dict = json.loads(dict_country)
